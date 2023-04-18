8d4be3966bf042872b5c061d6717aa7425fa550b
lilomox <sadesher@student.21-school.ru>
2023-04-18 18:37:57 +0700

    make a clang-format for all C-files

--- src/libgeom/checknput.h
+++ src/libgeom/checknput.h
@@ -3,19 +3,17 @@
 #define ERROR_IN_SYNTAX 12
 #define ERROR_IN_ARGUE 13
 
-typedef struct IntVector {
+typedef struct CharVector {
     char* data;
     size_t size;
     size_t capacity;
-} IntVector;
+} CharVector;
 
-IntVector* int_vector_new(size_t capacity);
+CharVector* char_vector_new(size_t capacity);
 
-int int_vector_push_back(IntVector* v, char item);
+int char_vector_push_back(CharVector* v, char item);
 
-void p_arr(double* arr, int n);
-
-void int_vector_free(IntVector* v);
+void char_vector_free(CharVector* v);
 
 int check_syntax(char* mass, int len);
 
---
+++ src/libgeom/intersection_of_circles.h
@@ -0,0 +1,7 @@
+typedef struct Circle {
+    double x;
+    double y;
+    double radius;
+} Circle;
+
+int intersection_of_circles(Circle* circle1, Circle* circle2);
